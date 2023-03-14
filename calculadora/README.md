1bc72e9dc487a3a4707f7cd13e6932b821553447
PatyGuimaraes <patriciabguimaraes@gmail.com>
2023-03-14 18:53:21 -0300

    Adiciona exemplo sobre Documentação

---
+++ calculadora/README.md
@@ -0,0 +1,5 @@
+## Calculadora
+
+Código para visualizar como funciona a documentação.
+
+O mouse em cima de um método ou da classe demonstrará dados da documentação.
---
+++ calculadora/src/Comentarios.java
@@ -0,0 +1,22 @@
+public class Comentarios {
+    // Olá, eu sou um comentário em uma única linha
+
+    /*
+     * Olá,
+     * Eu sou um comentario
+     * que posso ser mais detalhado
+     * quando necessário
+     */
+
+    /**
+     * Estas duas estrelinhas acima
+     * é para identificar que você
+     * pretende elaborar um comentário
+     * a nível de documentação.
+     * Que incrível !!!
+     */
+
+     public void metodo(){
+
+     }
+}
