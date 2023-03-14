61cba11df82484f6bc77bb3c55b87a2ac90e3a6a
PatyGuimaraes <patriciabguimaraes@gmail.com>
2023-03-13 21:25:45 -0300

    Adiciona tipos variaveis

--- tipos-variaveis/src/TiposVariaveis.java
+++ tipos-variaveis/src/TiposVariaveis.java
@@ -3,10 +3,10 @@ public class TiposVariaveis {
     public static void main(String[] args) {
         // TiposEVariaveis.java
 
-        short numeroCurto = 1;
+       /*short numeroCurto = 1;
         int numeroNormal = numeroCurto;
         short numeroCurto2 = (short) numeroNormal; // Cast para ter mais precisão e não dar erro
-
+        */
         int numero = 5;
         numero = 10;
         System.out.print(numero);
